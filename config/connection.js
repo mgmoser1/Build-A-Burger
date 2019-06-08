156fcef2806947af55b3b31b3b0e85ec409150c9
MGM <mgmoser1@users.noreply.github.com>
2019-06-07 19:44:38 -0500

    created file tree, database

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+/*
+setup the code to connect Node to MySQL.
+Export the connection.
+
+*/
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Gruyere Burger');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Onion Beyond Burger');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+
